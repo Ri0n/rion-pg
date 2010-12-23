e4ff7cf6765a199adad6dc2669aec3e48e6ef2ab
Ri0n <rion4ik@gmail.com>
2010-12-23 08:09:06 +0000

    fix comparision warning

--- rdns/domainname.cpp
+++ rdns/domainname.cpp
@@ -10,7 +10,7 @@ DomainName::DomainName()
 size_t DomainName::fromByteArray(const unsigned char *buf, size_t count)
 {
 	_name.reserve(count > 255 ? 255 : count); // 255 name limit by rfc
-	int c = 0;
+	unsigned int c = 0;
 	while (count - c > 0 && buf[c] && c + buf[c] + 1 < count) {
 		_name += std::string((const char*)&buf[c + 1], (size_t)buf[c]);
 		c += buf[c] + 1;
